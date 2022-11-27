245c3402dc6b5fc2db2a4be916de5d4dc9fab137
Jhonnatan Espinoza <hytzy18@gmai.com>
2022-11-26 22:29:53 -0500

    test

--- p2.cpp
+++ p2.cpp
@@ -8,24 +8,27 @@
 int main(int argc, char **argv) 
 {
 	int rank, size;
-	int matrix[N*N];
+	int A[N*N];
 	int vector[N];
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	int row[N];
 	if(rank == 0)
 	{
 		// obtenemos los valores de A
 		srand(time(NULL));
 		for( int i = 0; i<N*N; i++)
-			matrix[i] = i;//rand()%64 + 1;
+			A[i] = i+1;//rand()%64 + 1;
 		// obtenemos los valores de v
 		for( int i = 0; i<N; i++)
 			vector[i] = rand()%64 +1;
-		// enviar filas 
+		// enviar filas de A
+		MPI_Scatter(&A,N,MPI_INT,row,1,MPI_INT,0,MPI_COMM_WORLD);
 	}
+	//imprimo matrices en cada 
 	for( int i = 0; i<N*N; i++)
-		printf("%d ",matrix[i]);
+		printf("%d ",row[i]);
 	printf("\n");
 	MPI_Finalize();
 }
