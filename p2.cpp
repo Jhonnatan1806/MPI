77ef2ac4eb8ba04f6a05d33f548986d982e39b28
Jhonnatan Espinoza <hytzy18@gmai.com>
2022-11-26 21:41:42 -0500

    update

--- p2.cpp
+++ p2.cpp
@@ -3,7 +3,16 @@
 # include <stdio.h>
 # include <stdlib.h>
 
+# define N 4
+
 int main(int argc, char **argv) 
 {
-	
+	int rank, size;
+	int matrix[N][N];
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	matrix[0][0] = 0;
+	printf("Matriz \n",matrix[0][0]);
+	MPI_Finalize();
 }
