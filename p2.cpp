a4a7af9a41262a814c42b0961ec3d14a11694731
Jhonnatan Espinoza <hytzy18@gmai.com>
2022-11-26 22:47:19 -0500

    update

--- p2.cpp
+++ p2.cpp
@@ -12,8 +12,9 @@ int main(int argc, char **argv)
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	int A[N*N];
-	int vector[N],row[N];
+	
+	int A[N*N], vector[N], row[N];
+	
 	if(rank == 0)
 	{
 		// obtenemos los valores de A
@@ -22,14 +23,26 @@ int main(int argc, char **argv)
 			A[i] = i+1;//rand()%64 + 1;
 		// obtenemos los valores de v
 		for( int i = 0; i<N; i++)
-			vector[i] = rand()%64 +1;
+			vector[i] = 2;//rand()%64 +1;
 	}
 
-	// enviar filas de A
-	MPI_Scatter(&A,N,MPI_INT,row,0,MPI_INT,0,MPI_COMM_WORLD);
+	// enviar datos del array A tomados de N en N
+	MPI_Scatter(&A,N,MPI_INT,row,N,MPI_INT,0,MPI_COMM_WORLD);
+
+	// enviar datos del vector
+	MPI_Bcast(&vector,N,MPI_INT,0,MPI_COMM_WORLD);
+
 	//imprimo matrices en cada 
-	for( int i = 0; i<N*N; i++)
+	for( int i = 0; i<N; i++)
 		printf("%d ",row[i]);
 	printf("\n");
+
+	if(rank == 0)
+	{
+		for( int i = 0; i<N; i++)
+			printf("%d ",row[i]*vector[i]);
+		printf("\n");
+	}
+
 	MPI_Finalize();
 }
