9399917dcda1aac541b219861f510490f7e264f1
Jhonnatan Espinoza <hytzy18@gmai.com>
2022-11-26 21:45:58 -0500

    update

--- p2.cpp
+++ p2.cpp
@@ -12,7 +12,8 @@ int main(int argc, char **argv)
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	matrix[0][0] = 0;
-	printf("Matriz %d\n",matrix[0][0]);
+	matrix[0][0] = 10;
+	matrix[0][1] = 11;
+	printf("Matriz %d\n",matrix[0]);
 	MPI_Finalize();
 }
