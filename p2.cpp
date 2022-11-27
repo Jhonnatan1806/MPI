3fed1f35d45e66cbed37c24c4d3a45c3b5f1a916
Jhonnatan Espinoza <hytzy18@gmai.com>
2022-11-26 22:36:16 -0500

    test

--- p2.cpp
+++ p2.cpp
@@ -12,7 +12,7 @@ int main(int argc, char **argv)
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	int A[N][N];
+	int A[N*N];
 	int vector[N],row[N];
 	if(rank == 0)
 	{
