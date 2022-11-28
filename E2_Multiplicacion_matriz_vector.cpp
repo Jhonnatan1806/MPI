c4cf0edd0da03d5bddff5b9cdd94812c910e408a
Jhonnatan Espinoza <hytzy18@gmai.com>
2022-11-28 18:07:58 -0500

    update

--- E2_Multiplicacion_matriz_vector.cpp
+++ E2_Multiplicacion_matriz_vector.cpp
@@ -1,8 +1,10 @@
 # include <iostream>
 # include <mpi.h>
 # include <stdio.h>
+# include <stdlib.h>
+# include <time.h>
 
-# define REP 1000
+# define N 16
 
 int main(int argc, char **argv) 
 {
@@ -12,43 +14,44 @@ int main(int argc, char **argv)
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	
-	const int N = size;
-
 	double t1, t2, t3, tp;
-	int A[N][N], vector[N], row[N], x[N];
-	int sum=0;
+	int A[N][N]; 
+	int vector[N], row[N/size][N], x[N];
+	int sum[N/size];
 	
 	if(rank == 0)
 	{
 		srand(time(NULL));
-		// generamos los valores de A
+		// generamos los valores de la matriz A
 		for(int i = 0; i<N; i++)
 			for(int j = 0; j<N; j++)
 				A[i][j] = rand()%64 + 1;
-		// generamos los valores de v
+		// generamos los valores de el vector v
 		for(int i = 0; i<N; i++)
 			vector[i] = rand()%4 +1;
 	}
 
-	// Broadcast p0 envia el vector v a p1,p2,...
+	// Broadcast p0 envia el vector v hacia p1,p2,...
 	t1 = MPI_Wtime();
 	MPI_Bcast(&vector,N,MPI_INT,0,MPI_COMM_WORLD);
 	t1 = MPI_Wtime() - t1;
 
-	// Scatter p0 envia las filas de A a p1,p2,...
+	// Scatter p0 envia k bloques de filas A hacia p1,p2,...
+	// donde k = N/#p
 	t2 = MPI_Wtime(); 
-	MPI_Scatter(&A,N,MPI_INT,row,N,MPI_INT,0,MPI_COMM_WORLD);
+	MPI_Scatter(&A,(N/size)*N,MPI_INT,row,(N/size)*N,MPI_INT,0,MPI_COMM_WORLD);
 	t2 = MPI_Wtime() - t2;
 
 	tp = MPI_Wtime(); 
 	// realizamos las sumas locales en p1,p2,...
-	for(int i = 0; i<N; i++)
-		sum += row[i]*vector[i];
+	for(int i = 0; i<(N/size); i++)
+		for(int j = 0; j<N; j++)
+			sum[i] += row[i][j]*vector[j];
 	tp = MPI_Wtime() - tp; 
 
 	// Gather p0 almacena las sumas locales en el array X
 	t3 = MPI_Wtime(); 
-	MPI_Gather(&sum,1,MPI_INT,x,1,MPI_INT,0,MPI_COMM_WORLD);
+	MPI_Gather(&sum,(N/size),MPI_INT,x,1,MPI_INT,0,MPI_COMM_WORLD);
 	t3 = MPI_Wtime() - t3; 
 
 	// Imprimir el vector x[] en p0
