fc10c9a44aad4896b70be96a88fb86dd727b36ee
Jhonnatan Espinoza <hytzy18@gmai.com>
2022-11-28 12:49:10 -0500

    update

--- p2.cpp
+++ p2.cpp
@@ -35,7 +35,6 @@ int main(int argc, char **argv)
 	MPI_Scatter(&A,N,MPI_INT,&row,N,MPI_INT,0,MPI_COMM_WORLD);
 
 	// realizamos suma en cada proceso
-	printf("Rank %d:\n",rank);
 	for(int i = 0; i<N; i++)
 		sum += row[i]*vector[i];
 
