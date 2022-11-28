d27b1ab3206298ab265d1245a133420fa0a9bbb3
Jhonnatan Espinoza <hytzy18@gmai.com>
2022-11-28 17:11:02 -0500

    update

--- E2_Multiplicacion_matriz_vector.cpp
+++ E2_Multiplicacion_matriz_vector.cpp
@@ -2,6 +2,8 @@
 # include <mpi.h>
 # include <stdio.h>
 
+# define REP 1000
+
 int main(int argc, char **argv) 
 {
 	int rank, size;
@@ -12,6 +14,7 @@ int main(int argc, char **argv)
 	
 	const int N = size;
 
+	double t1, t2, t3;
 	int A[N][N], vector[N], row[N], x[N];
 	int sum=0;
 	
@@ -28,17 +31,23 @@ int main(int argc, char **argv)
 	}
 
 	// Broadcast p0 envia el vector v a p1,p2,...
+	t1 = MPI_Wtime();
 	MPI_Bcast(&vector,N,MPI_INT,0,MPI_COMM_WORLD);
+	t1 -= MPI_Wtime();
 
-	// Scatter p0 envia las filas de A a p1,p2,... 
+	// Scatter p0 envia las filas de A a p1,p2,...
+	t2 = MPI_Wtime(); 
 	MPI_Scatter(&A,N,MPI_INT,row,N,MPI_INT,0,MPI_COMM_WORLD);
+	t2 -= MPI_Wtime();
 
 	// realizamos las sumas locales en p1,p2,...
 	for(int i = 0; i<N; i++)
 		sum += row[i]*vector[i];
 
 	// Gather p0 almacena las sumas locales en el array X
+	t3 = MPI_Wtime(); 
 	MPI_Gather(&sum,1,MPI_INT,x,1,MPI_INT,0,MPI_COMM_WORLD);
+	t3 -= MPI_Wtime(); 
 
 	// Imprimir el vector x[] en p0
 	if(rank == 0)
@@ -47,6 +56,9 @@ int main(int argc, char **argv)
 		for (int i = 0; i<N; i++)
         	printf("%d ", x[i]);
         printf("]\n");
+        printf("Tiempo Comm Bcast: %f\n",t1/REP);
+        printf("Tiempo Comm Scatter: %f\n",t2/REP);
+        printf("Tiempo Comm Gather: %f\n",t3/REP);
 	}
 
 	MPI_Finalize();
