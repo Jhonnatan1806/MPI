ff10172d3653b88176b093aeea1f5bc693e46c21
Jhonnatan Espinoza <hytzy18@gmai.com>
2022-11-30 22:21:28 -0500

    update

--- E3_Adivina_mi_numero_MPI.cpp
+++ E3_Adivina_mi_numero_MPI.cpp
@@ -17,7 +17,7 @@ int main(int argc, char **argv)
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-	for(int i = 0; i<3 ;i++)
+	for(int i = 0; i<REP ;i++)
 	{
 	if (rank == 0)
 	{
